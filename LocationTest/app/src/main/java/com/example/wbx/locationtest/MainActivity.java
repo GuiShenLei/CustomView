0d101b45529c23a019ca189ca9326eb87e111361
wbx <18867521887@139.com>
2018-08-13 18:23:58 +0800

    fix #new location project Signed-off-by: wbx <18867521887@139.com>

---
+++ LocationTest/app/src/main/java/com/example/wbx/locationtest/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.wbx.locationtest;
+
+import android.content.Context;
+import android.location.LocationManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView textView;
+    private LocationManager locationManager;
+    private String provider;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.text);
+
+        locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+    }
+}
